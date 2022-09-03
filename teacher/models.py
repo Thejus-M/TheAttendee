2a85c2d9126e288abf6803c4f00c9112318f2ccf
Thejus <git@tigerraw22.anonaddy.com>
2022-09-03 21:50:53 +0530

    a ok model

--- teacher/models.py
+++ teacher/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class student(models.Model):
+    rollno = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=20)
+    class_section = models.CharField(max_length=10)
+
+class Attendant(models.Model):
+    student = models.ForeignKey(student, on_delete=models.CASCADE)
+    attendants = models.BooleanField()
+    subject = models.CharField(max_length=10)
+    period_no = models.IntegerField()
+    time = models.DateTimeField(auto_now=True)
